7d99bc5dd87ed73d2bdbb124bb9e0a17649bf5e9
RavivarmaJampani <127435982+RavivarmaJampani@users.noreply.github.com>
2023-04-28 23:34:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,16 @@ if(req.url === '/'){
  )
  
 }
+else if(req.url ==='/about.html'){
+
+       
+    fs.readFile(path.join(__dirname,'public','about.html'),
+    (err, content)=>{
+        if(err ) throw err;
+        res.writeHead(200, { 'Content-type': 'text/html'})
+        res.end(content)
+    });
+}
 else if(req.url=='/api')
 {
 
